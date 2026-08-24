295b061608a6685b8e04c0283e58b00f6d64519e
Xenderasufie987 <116882035+Xenderasufie987@users.noreply.github.com>
2026-08-24 09:57:06 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Isi konfigurasi dari Firebase Console.
+// Jangan menghapus struktur object di bawah.
+export const firebaseConfig = {
+  apiKey: "GANTI_API_KEY",
+  authDomain: "GANTI_PROJECT.firebaseapp.com",
+  projectId: "GANTI_PROJECT_ID",
+  storageBucket: "GANTI_PROJECT.appspot.com",
+  messagingSenderId: "GANTI_SENDER_ID",
+  appId: "GANTI_APP_ID"
+};
