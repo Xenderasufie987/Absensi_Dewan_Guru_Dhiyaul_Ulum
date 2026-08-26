21eaa3c0f578ef4baca0f8050ecd3de0dbca5561
Xenderasufie987 <116882035+Xenderasufie987@users.noreply.github.com>
2026-08-26 11:06:13 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Isi konfigurasi dari Firebase Console.
+// Jangan menghapus struktur object di bawah.
+const firebaseConfig = {
+  apiKey: "AIzaSyAljG_IorAvqcwbUXyEu_P7bcuJdNH30yk",
+  authDomain: "absensi-dewan-guru-dhiyaululum.firebaseapp.com",
+  projectId: "absensi-dewan-guru-dhiyaululum",
+  storageBucket: "absensi-dewan-guru-dhiyaululum.firebasestorage.app",
+  messagingSenderId: "501013302609",
+  appId: "1:501013302609:web:312b548db38a9c351f5770",
+  measurementId: "G-GZ3KYWNVTV"
+};
